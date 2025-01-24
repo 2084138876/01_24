338da4bad555174f43cc930ed765756b34b6481d
2084138876 <2084138876@qq.com>
2025-01-24 22:54:04 +0800

    Add files via upload

---
+++ 用递归求第n个斐波那契数.c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int func(int n)
+//{
+//	if (n <= 2)
+//	{
+//		return 1;
+//	}
+//	else
+//	{
+//		return func(n - 1) + func(n - 2);
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	int ret = func(n);
+//	scanf("%d", n);
+//	printf("第%d个斐波那契数是：%d\n", n, ret);
+//
+//	return 0;
+//}
